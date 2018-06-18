61eaf3f63427cf07fa5cd8fb7cb8b89e6890f51c
Ayappa Reddy <aayappareddy@gmail.com>
2018-06-19 03:46:57 +0530

    Updated UI controller to display all the items in the state container along with the total calories

--- app.js
+++ app.js
@@ -37,15 +37,47 @@ const ItemCtrl = (function ItemCtrl() {
 })();
 
 // UI Controller
-const UICtrl = (function UICtrl() {})();
+const UICtrl = (function UICtrl() {
+  UISelectors = {
+    itemListEl: ".item-list",
+    totalCaloriesEl: ".heading-primary__total-calories"
+  };
+
+  return {
+    displayItems(items) {
+      let html = "";
+
+      items.forEach(item => {
+        html += `
+        <li class="item-list__item">
+          <strong class="item-list__meal">${item.name}:
+          </strong>
+          <em class="item-list__calories">${item.calories} Calories</em>
+          <span class="item-list__edit-item">E</span>
+        </li>
+        `;
+      });
+
+      document.querySelector(UISelectors.itemListEl).innerHTML = html;
+    },
+
+    displayTotalCalories(totalCalories) {
+      document.querySelector(
+        UISelectors.totalCaloriesEl
+      ).innerHTML = totalCalories;
+    }
+  };
+})();
 
 // APP Controller
 const App = (function App(StorageCtrl, ItemCtrl, UICtrl) {
+  const items = ItemCtrl.getItems();
+
+  const totalCalories = ItemCtrl.getTotalCalories();
   return {
     init() {
-      const items = ItemCtrl.getItems();
-      const totalCalories = ItemCtrl.getTotalCalories();
-      console.log(items);
+      UICtrl.displayItems(items);
+      UICtrl.displayTotalCalories(totalCalories);
       console.log(totalCalories);
     }
   };
